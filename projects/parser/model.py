0fdeaf2cee320004c8c5cb41ddf5f4b2ed928313
abobaaaaaaaaa <151593066+abobaaaaaaaaa@users.noreply.github.com>
2024-04-17 11:51:41 +0700

    Update model.py

--- projects/parser/model.py
+++ projects/parser/model.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+import numpy as np
+
 
 class Parser:
     def __init__(self, page, container, class_container):
@@ -21,7 +24,8 @@ class Parser:
             response = self.session.get(self.page)
             response.raise_for_status()
             bs = BeautifulSoup(response.text, 'html.parser')
-            result = bs.find(self.class_container, self.container)
-            return result.text if result else None
+            result = bs.find_all(self.class_container, self.container)
+            for i in range(len(result)):
+                return result[i].text if result else None
         except requests.exceptions.RequestException as e:
             return str(e)
